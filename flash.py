bb3d0a67f8f70c9116d448a8471e18f68768f2e3
Ludvig Linde <ludviglinde@outlook.com>
2023-04-28 10:19:59 +0200

    3

--- flash.py
+++ flash.py
@@ -1,6 +1,7 @@
 from flask import *
 from cryptography.fernet import Fernet
 import os
+from waitress import serve
 
 key = Fernet.generate_key()
 fernet = Fernet(key)
@@ -11,58 +12,54 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-
-    temp = os.listdir(os.path.join("static","templates"))
+    temp = os.listdir(os.path.join("static", "templates"))
 
     templates = {}
-    
+
     for x in temp:
-        with open(os.path.join("static","templates",x),encoding="utf-8") as f:
+        with open(os.path.join("static", "templates", x), encoding="utf-8") as f:
             content = f.read()
-        templates[x] = content.replace("\n","\\n")
+        templates[x] = content.replace("\n", "\\n")
 
     print(templates)
-    
-    return render_template("index.html",templates=templates)
 
-@app.route("/encrypt", methods=["GET","POST"])
+    return render_template("index.html", templates=templates)
+
+
+@app.route("/encrypt", methods=["GET", "POST"])
 def encrypt():
-
     message = request.form["message"]
 
     encrypt = fernet.encrypt(message.encode())
 
-    with open(os.path.join("static","messages",encrypt.decode()),"w") as f:
+    with open(os.path.join("static", "messages", encrypt.decode()), "w") as f:
         f.write("")
 
     link = "/message?file=" + encrypt.decode()
-    
-    return render_template("encrypt.html",link=link)
+
+    return render_template("encrypt.html", link=link)
+
 
 @app.route("/message")
 def message():
+    message = "/view?file=" + request.args.get("file")
 
-    message = "/view?file=" + request.args.get('file')
+    return render_template("message.html", message=message)
 
-    
-        
-    return render_template("message.html",message=message)
 
 @app.route("/view")
 def view():
+    file = request.args.get("file")
 
-    file = request.args.get('file')
-    
     message = None
 
-    if os.path.isfile(os.path.join("static","messages",file)):
-        os.remove(os.path.join("static","messages",file))
+    if os.path.isfile(os.path.join("static", "messages", file)):
+        os.remove(os.path.join("static", "messages", file))
         message = fernet.decrypt(file.encode()).decode()
-    
-        
-    return render_template("view.html",message=message)
 
+    return render_template("view.html", message=message)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port="80")
+    # app.run(host="0.0.0.0", port="8080")
+    serve(app, host="0.0.0.0", port=8080)
